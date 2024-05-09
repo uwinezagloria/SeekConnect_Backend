f149c852401e7f2407f7ad5142f395553a4cb722
UWINEZA7Gloria <gloria7wineza@gmail.com>
2024-05-09 13:10:26 +0200

    signup and otp API

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express, { Router } from "express"
+import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
 import { errorHandler } from "./middlewares/errorHandler.js"
