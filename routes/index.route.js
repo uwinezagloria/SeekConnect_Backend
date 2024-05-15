ecc6340e8f47560a546d68b3398e6a5b966c3e9d
UWINEZA7Gloria <gloria7wineza@gmail.com>
2024-05-15 13:30:52 +0200

    CONTACTUS MODEL,CONTROLLR AND SWAGGER DOCUMENTATION

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,7 +1,10 @@
 import express from "express"
-import { forgotPasswordValidation, loginValidation, lostDocumentValidation, signUpValidation } from "../middlewares/validations.js"
+import { contactUsValidation, forgotPasswordValidation, loginValidation, lostDocumentValidation, missingPersonValidation, signUpValidation } from "../middlewares/validations.js"
 import { signUp, verifyOtp, login, resertNewPassword} from "../controllers/user.controller.js"
 import { createLostDocument, deleteLostDocument, getLostDocuments, updateLostDocument } from "../controllers/lostDocumentController.js"
+import { getMissingPeople, postMissingPerson, removeMissingPerson, updateMissingPerson } from "../controllers/missingPersonController.js"
+import upload from "../middlewares/files.js"
+import { createContactUs, deleteContactUs, getContact, getContactUs, updateContactUs } from "../controllers/contactUs.controller.js"
 const router=express.Router()
 //signUp && signIn route
 router.route("/signUp").post(signUpValidation,signUp)
@@ -13,4 +16,15 @@ router.route("/lost").post(lostDocumentValidation,createLostDocument)
 router.route("/lost").get(getLostDocuments)
 router.route("/lost").patch(updateLostDocument)
 router.route("/lost").delete(deleteLostDocument)
+//MissingPerson route
+router.route("/missingPerson").post(missingPersonValidation,upload.array("Photo"),postMissingPerson)
+router.route("/missingPerson").patch(updateMissingPerson)
+router.route("/missingPeople").get(getMissingPeople)
+router.route("/missingPerson").delete(removeMissingPerson)
+//contact us route
+router.route("/contactUs").post(contactUsValidation,createContactUs)
+router.route("/contactUs").get(getContactUs)
+router.route("/contactUs/email").get(getContact)
+router.route("/contactUs").patch(updateContactUs)
+router.route("/contactUs").delete(deleteContactUs)
 export default router 
